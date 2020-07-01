a4d0f8285199bc631124ce30d9e8bfd7954aa74a
Eric Wang <johnericcw@gmail.com>
2020-07-01 18:25:49 -0400

    Some hiding of keys, loading Drive API correctly

--- src/popup.js
+++ src/popup.js
@@ -1,18 +1,25 @@
+import gapi from 'gapi';
+
 import './popup.html';
+import keys from '../keys.json';
 
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', (data) => {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = (element) => {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        chrome.tabs.executeScript(
-            tabs[0].id, 
-            {code: `document.body.style.backgroundColor = "${color}";`}
+window.onload = () => {
+    document.querySelector('button').addEventListener('click', () => {
+        gapi.client.init({
+            apiKey: keys.API_KEY,
+            discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+        }).then(
+            gapiAuth,
+            (error) => console.log('error', error)
         );
     });
 };
+
+function gapiAuth()
+{
+    console.log('gapi initialized');
+    chrome.identity.getAuthToken({interactive: true}, (token) => {
+        gapi.auth.setToken({'access_token': token});
+        console.log('token set');
+    });
+}
