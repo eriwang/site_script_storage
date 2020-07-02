b85183c92710c716fc9e22bd3c1499a6fa29a840
Eric Wang <johnericcw@gmail.com>
2020-07-01 21:08:42 -0400

    Hacked in proof of concept, able to select a file from Drive and execute it as a content script on a webpage

--- src/popup.js
+++ src/popup.js
@@ -3,14 +3,20 @@ import gapi from 'gapi';
 import './popup.html';
 import keys from '../keys.json';
 
+// The "google" variable is loaded by the gapi.load('picker') call. Unfortunately, making it an external in webpack
+// messes with the module imports since "google" isn't defined yet, so we go with a magical global variable instead.
+
+// TODO: lots of bad race conditions here with script loading.
 window.onload = () => {
-    document.querySelector('button').addEventListener('click', () => {
+    gapi.load('picker');
+
+    document.getElementById('press').addEventListener('click', () => {
         gapi.client.init({
             apiKey: keys.API_KEY,
             discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
         }).then(
             gapiAuth,
-            (error) => console.log('error', error)
+            (error) => console.error(error)
         );
     });
 };
@@ -21,5 +27,36 @@ function gapiAuth()
     chrome.identity.getAuthToken({interactive: true}, (token) => {
         gapi.auth.setToken({'access_token': token});
         console.log('token set');
+        let view = new google.picker.DocsView()
+            .setIncludeFolders(true)
+            .setSelectFolderEnabled(true)
+            .setMode(google.picker.DocsViewMode.GRID)
+            .setMimeTypes('text/javascript');
+        let picker = new google.picker.PickerBuilder()
+            .enableFeature(google.picker.Feature.NAV_HIDDEN)
+            .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+            .setAppId(keys.APP_ID)
+            .setOAuthToken(token)
+            .addView(view)
+            .setDeveloperKey(keys.API_KEY)
+            .setCallback(echoFile)
+            .build();
+        picker.setVisible(true);
     });
+}
+
+function echoFile(data)
+{
+    if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED)
+    {
+        const doc = data[google.picker.Response.DOCUMENTS][0];
+        const docId = doc[google.picker.Document.ID];
+        console.log(`picked ${docId}`);
+        gapi.client.drive.files.get({'fileId': docId, 'alt': 'media'})
+            .then((data) => {
+                chrome.tabs.executeScript({
+                    'code': data['body']
+                });
+            });
+    }
 }
--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
         'path': path.resolve(__dirname, 'gen/')
     },
 
-    externals: {
+    'externals': {
         'gapi': 'gapi'
     },
 
